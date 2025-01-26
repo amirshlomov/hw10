bc771ac8805a2ebcd5ecbc534b43222b177d0e08
amirshlomov <amir.shlomov@gmail.com>
2025-01-26 17:04:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,33 @@ let batman=document.getElementById('summ');
 
 let amount=0;
 
+function checkOverDraft(amount) {
+    let warning=document.getElementById('warning');
+
+    if (amount < 0) {
+        warning.innerHTML = "אתה במינוס!";
+    }
+    else {
+        warning.innerHTML = "";
+    }
+}
+
 function add(){
     amount=amount+1;
     batman.innerHTML=amount;
+
+    checkOverDraft(amount);
 }
 
 function take(){
     amount=amount-1;
-    if (amount < 0) {
-        alert("אתה לא יכול להכנס למינוס");
-    }
-    else {
-        batman.innerHTML=amount;
-    }
+    batman.innerHTML=amount;
+    checkOverDraft(amount);
 }
 
 
 function zero(){
     amount=amount*0;
     batman.innerHTML=amount;
+    checkOverDraft(amount);
 }
